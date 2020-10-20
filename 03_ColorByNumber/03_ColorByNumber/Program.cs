ba0c0421587a4dc7e7b59f53a75312ddbf6e1b83
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-10-20 19:27:08 +0200

    added 03 exercises

---
+++ 03_ColorByNumber/03_ColorByNumber/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ColorByNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for(int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = Convert.ToChar(input[j]);
+                }
+            }
+
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            char value = matrix[getPosition(start, n)[0], getPosition(start, n)[1]];
+            ColorByNumber(matrix, start, value, n);
+
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        public static void ColorByNumber(char[,] matrix, int start, char value, int n)
+        {           
+            foreach(int position in getAdjacent(start, n))
+            {
+                if(matrix[getPosition(position, n)[0], getPosition(position, n)[1]] == value)
+                {
+                    matrix[getPosition(position, n)[0], getPosition(position, n)[1]] = 'X';
+                    ColorByNumber(matrix, position, value, n);
+                }
+            }
+        }
+
+        public static List<int> getAdjacent(int start, int n)
+        {
+            List<int> adjacent = new List<int>();
+            //top
+            if (start >= n)
+            {
+                adjacent.Add(start - n);
+            }
+            //left
+            if (start % n != 0)
+            {
+                adjacent.Add(start - 1);
+            }
+            //bottom
+            if (start < (n * n) - n)
+            {
+                adjacent.Add(start + n);
+            }
+            //right
+            if (start % n != n -1)
+            {
+                adjacent.Add(start + 1);
+            }
+
+            return adjacent;
+        }
+
+        public static int[] getPosition(int start, int n)
+        {
+            return new int[] { start / n, start % n };
+        }
+    }
+}
---
+++ 03_WeHebbenConnectie/03_WeHebbenConnectie/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_WeHebbenConnectie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nodes = Convert.ToInt32(Console.ReadLine());
+            int connections = Convert.ToInt32(Console.ReadLine());
+            List<int>[] graph = new List<int>[nodes];
+
+            for(int i = 0; i < connections; i++)
+            {                
+                graph[i] = new List<int>();
+            }
+
+            for(int i = 0; i < connections; i++)
+            {
+                string[] connection = Console.ReadLine().Split();
+                graph[Convert.ToInt32(connection[0])].Add(Convert.ToInt32(connection[1]));
+                graph[Convert.ToInt32(connection[1])].Add(Convert.ToInt32(connection[0]));
+            }
+
+            isConnected(graph);
+        }
+
+        static void isConnected(List<int>[] graph, int node = 0)
+        {
+            bool[] visited = new bool[graph.Length];
+            Queue<int> queue = new Queue<int>();
+
+            visited[node] = true;
+            queue.Enqueue(node);
+
+            while(queue.Count != 0)
+            {
+                node = queue.Dequeue();
+
+                foreach(int connection in graph[node])
+                {
+                    if(!visited[connection])
+                    {
+                        visited[connection] = true;
+                        queue.Enqueue(connection);
+                    }
+                }
+            }
+
+            if(visited.Contains(false))
+            {
+                Console.WriteLine("not connected");
+            }
+            else
+            {
+                Console.WriteLine("connected");
+            }
+        }
+    }
+}
